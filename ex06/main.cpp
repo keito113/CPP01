01254f52c7cce39797e90ba25a236ebc8a68ff52
keito113 <keito1440@gmail.com>
2026-03-23 19:39:34 +0900

    ex06 compleat

---
+++ ex06/main.cpp
@@ -0,0 +1,12 @@
+#include "Harl.hpp"
+#include <iostream>
+
+int main(int ac, char *av[]){
+	Harl harl;
+	if(ac != 2){
+		harl.complain("");
+		return(0);
+	}
+	harl.complain(av[1]);
+	return(0);
+}
